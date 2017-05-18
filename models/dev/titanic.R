b104fdc7335c58bb0651de976fd0c4a0a2d37a04
robertzk <technoguyrob@gmail.com>
2017-05-17 23:50:26 -0500

    Titanic model mid data stage.

--- models/dev/titanic.R
+++ models/dev/titanic.R
@@ -1,8 +1,8 @@
 titles <- c(
-  mister = "Mr.", missus = "Mrs.", miss = "Ms\\.|Miss\\.",
+  mr = "Mr.", ms = "Mrs.", mrs = "Ms\\.|Miss\\.",
   master = "Master.", rev = "Rev.", dr = "Dr."
 )
-fixed_titles <- c("mister", "missus", "master", "rev", "dr")
+fixed_titles <- c("mr", "ms", "master", "rev", "dr")
 
 list(
   import = list(
@@ -15,7 +15,10 @@ list(
   data = list(
     "has paren in name" = list(multi_column_transformation(function(name) grepl("(", fixed = TRUE, name)), "name", "has_paren")
    ,"Name length variable" = list(new_variable, function(name) nchar(name), "name_length")
-   ,"Formal title" = list(regex_factor, "name", "formal_title", cases = titles, fixed = fixed_titles)
+   ,"Formal title" = list(regex_factor, "name", "title", cases = titles, fixed = fixed_titles)
+   ,"title_fare variable" = list(new_variable, function(title, fare) { stats::ave(fare, title, FUN = mean) }, "title_fare")
+   ,"class_fare"           = list(multi_column_transformation(function(klass, fare) { ave(fare, klass, FUN = mean) }), c("pclass", "fare"), "class_fare")
+   ,"Some simple derivations" = list(atransform, alist(fare_diff = fare - title_fare, fare_pct = fare / title_fare, fare_diff_class = fare - class_fare, fare_pct_class = fare / class_fare))
   )
 )
 
